35cc9e9774c56e5eba6ec1ef0e536636dd1c07cd
Edward Aylmer <mail@tedaylmer.co.uk>
2024-08-01 08:20:04 +0000

    feat: add basic views, urls, and models

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,13 @@
 from . import views
 from django.urls import path
+from .views import todo_list, delete_task, update_task
 
-urlpatterns = [ 
-path('', views.HomePage.as_view(), name='home'), 
+urlpatterns = [
+    path('', todo_list, name='todo_list'),
+    path('delete/<str:task_name>/', delete_task, name='delete'),
+    path('update/<str:task_name>/', update_task, name='update'),
 ]
+
+# urlpatterns = [ 
+#     path('', views.HomePage.as_view(), name='home'), 
+# ]
