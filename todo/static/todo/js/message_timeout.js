9e617efe9efb040693f4980c37805929c5679d2e
Edward Aylmer <mail@tedaylmer.co.uk>
2024-08-07 17:06:32 +0000

    add dialogue on delete function

--- todo/static/todo/js/message_timeout.js
+++
@@ -1,7 +0,0 @@
-// Auto-hide messages after 105 seconds
-setTimeout(function() {
-    var messages = document.querySelector('.messages');
-    if (messages) {
-        messages.style.display = 'none';
-    }
-}, 10000); // milliseconds
