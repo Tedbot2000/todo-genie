4e9006bf55b1c15d03fd5d42e376c6127145f1f4
Edward Aylmer <mail@tedaylmer.co.uk>
2024-08-13 13:07:28 +0000

    chore: deploy to Heroku

--- todo_genie/settings.py
+++ todo_genie/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
 ALLOWED_HOSTS = ['.codeinstitute-ide.net','.herokuapp.com']
 
@@ -101,6 +101,7 @@ WSGI_APPLICATION = 'todo_genie.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) 
