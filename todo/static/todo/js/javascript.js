5cdb849d805edee4800937b4850f5a6614869b1a
Edward Aylmer <mail@tedaylmer.co.uk>
2024-08-14 08:46:25 +0000

    fix(js) jaascript errors from JSLint

--- todo/static/todo/js/javascript.js
+++ todo/static/todo/js/javascript.js
@@ -1,27 +1,27 @@
-// Auto-hide messages after 5 seconds
-setTimeout(function() {
-    var messages = document.querySelector('.messages');
-    if (messages) {
-        messages.style.display = 'none';
-    }
-}, 5000); // milliseconds
-
-
+/*jshint esversion: 6, laxlet: true */
+/*global setTimeout, document, $, window */
 
 // Confirm the user wishes to delete the record.
-// If not - do nothing.
-let deleteUrl = ''; // used to redirect the user after they confirm deletion (in case of multiple pages)
+var deleteUrl = ""; // used to redirect the user
 
 function confirmDeleteAndRedirect(url) {
     deleteUrl = url;
-    $('#confirmModal').modal('show');
+    $("#confirmModal").modal("show");
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('confirmDeleteBtn').onclick = function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("confirmDeleteBtn").onclick = function () {
         window.location.href = deleteUrl;
     };
 });
+
+// Auto-hide messages after 5 seconds
+setTimeout(function () {
+    var messages = document.querySelector(".messages");
+    if (messages) {
+        messages.style.display = "none";
+    }
+}, 5000); // milliseconds
 
 // Toggle status buttons
 function toggleStatus(url) {
